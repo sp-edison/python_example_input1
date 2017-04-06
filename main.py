74a84c90989809426fbfa7227123360f6f8ad99c
sp-edison <edison@kisti.re.kr>
2017-04-06 15:03:12 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python
+
+
+import sys
+import getopt
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"i:")
+except getopt.GetoptError as err:
+    print str(err)
+    sys.exit(1)
+
+for opt,arg in opts:
+    if opt in ("-i"):
+        f_inputdeck = open(arg, "r")
+
+print "input file = " + f_inputdeck.name
+inputdeck_lines = f_inputdeck.readlines()
+
+
+f_inputdeck.close()
